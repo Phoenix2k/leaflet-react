4582b85ec2a5ad1dfd455f417912c5f18ccc8fb7
Niko Pettersen <niko.pettersen@gmail.com>
2020-04-23 19:03:27 +0300

    feat(map): Add more examples

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -36,6 +36,41 @@ const MapPage: NextPage = () => {
     lng: 24.9381,
     markers: [
       {
+        color: 'blue',
+        popup: (
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Centro_Musical_de_Helsinki%2C_Finlandia%2C_2012-08-14%2C_DD_01.JPG/1280px-Centro_Musical_de_Helsinki%2C_Finlandia%2C_2012-08-14%2C_DD_01.JPG"
+                title="Helsinki Music Centre"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Helsinki Music Centre
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  The Helsinki Music Centre (Finnish: Helsingin musiikkitalo, Swedish: Musikhuset i
+                  Helsingfors) is a concert hall and a music center in Töölönlahti, Helsinki. The
+                  building is home to Sibelius Academy and two symphony orchestras, the Finnish
+                  Radio Symphony Orchestra and the Helsinki Philharmonic Orchestra.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                href="https://en.wikipedia.org/wiki/Helsinki_Music_Centre"
+                rel="noopener noreferrer"
+                size="small"
+                target="_blank">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ),
+        position: [60.1737, 24.9349]
+      },
+      {
         color: 'green',
         popup: (
           <Card className={classes.root}>
@@ -84,6 +119,76 @@ const MapPage: NextPage = () => {
           </Card>
         ),
         position: [60.1718, 24.9392]
+      },
+      {
+        color: 'red',
+        popup: (
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Kansallismuseo_Helsinki.jpg/678px-Kansallismuseo_Helsinki.jpg"
+                title="National Museum of Finland"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  National Museum of Finland
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  The National Museum of Finland (Finnish: Kansallismuseo, Swedish: Nationalmuseum)
+                  presents Finnish history from the Stone Age to the present day, through objects
+                  and cultural history.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                href="https://en.wikipedia.org/wiki/National_Museum_of_Finland"
+                rel="noopener noreferrer"
+                size="small"
+                target="_blank">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ),
+        position: [60.1749, 24.9294]
+      },
+      {
+        color: 'yellow',
+        popup: (
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Helsinki_Central_Library_Oodi%2C_2019_%2801%29.jpg/800px-Helsinki_Central_Library_Oodi%2C_2019_%2801%29.jpg"
+                title="Helsinki Central Library Oodi, 2019"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Helsinki Central Library Oodi
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  The Helsinki Central Library Oodi (Finnish: Helsingin keskustakirjasto Oodi;
+                  Swedish: Helsingfors centrumbibliotek Ode), commonly referred to as Oodi (lit.
+                  'Ode'), is a public library in Helsinki, Finland, inaugurated on December 5, 2018.
+                  The library is situated in the Töölönlahti district next to Helsinki Music Centre
+                  and Kiasma Museum of Contemporary Art.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button
+                href="https://en.wikipedia.org/wiki/Helsinki_Central_Library_Oodi"
+                rel="noopener noreferrer"
+                size="small"
+                target="_blank">
+                Learn More
+              </Button>
+            </CardActions>
+          </Card>
+        ),
+        position: [60.1738, 24.9381]
       }
     ],
     maxZoom: 20,
